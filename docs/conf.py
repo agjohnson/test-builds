24e7224499b85da054d447b6dab2aec90fbedd4a
Anthony Johnson <aj@ohess.org>
2021-11-30 12:04:29 -0800

    Try colors

--- docs/conf.py
+++ docs/conf.py
@@ -11,3 +11,7 @@ latex_engine = 'xelatex'  # allow us to build Unicode chars
 
 # Include all your settings here
 html_theme = 'sphinx_rtd_theme'
+
+def setup(app):
+    import os
+    os.system('colortest')
